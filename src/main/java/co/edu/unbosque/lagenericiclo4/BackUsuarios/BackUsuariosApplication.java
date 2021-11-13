3146e61c743371d1aba53d80fec94dd157a63055
Alejandro Villa <alejandrovilla1705@gmail.com>
2021-11-13 05:03:55 -0500

    BackFull

---
+++ src/main/java/co/edu/unbosque/lagenericiclo4/BackUsuarios/BackUsuariosApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.lagenericiclo4.BackUsuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackUsuariosApplication.class, args);
+	}
+
+}
